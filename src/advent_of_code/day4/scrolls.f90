2bf39e50a28f7613f9336d13aaf9dee023b3c324
René van Klink <rene.vanklink@deltares.nl>
2026-01-14 12:25:57 +0000

    Read grid routine finished and added two tests

--- src/advent_of_code/day4/scrolls.f90
+++ src/advent_of_code/day4/scrolls.f90
@@ -1,4 +1,79 @@
 module m_scrolls
     use m_error, only: error_t
+    implicit none
+
+    private
+    integer, public, parameter :: MAX_COLUMNS = 256
+    integer, public, parameter :: MAX_ROWS = 256
+
+    integer, public, parameter :: MAX_ERROR_MESSAGE_LENGTH = 120
+    integer, public, parameter :: ERROR_READ_FAILED = -1
+    integer, public, parameter :: ERROR_INVALID_INPUT = -2
+
+    public :: read_scrolls
 contains
+    subroutine read_scrolls(unit, grid, rows, columns, error)
+        implicit none
+        integer, intent(in) :: unit
+        logical, allocatable, intent(out) :: grid(:,:)
+        integer, intent(out) :: rows, columns
+        type(error_t), allocatable, intent(out) :: error
+
+        logical :: temp_grid(MAX_ROWS, MAX_COLUMNS)
+        character(len=MAX_COLUMNS - 2) :: line
+        character(len=MAX_ERROR_MESSAGE_LENGTH) :: message
+        integer :: line_count, iostat, i, j
+
+        line_count = 1
+        read(unit, "(A)", iostat=iostat) line
+        if (iostat < 0) then
+            error = error_t(ERROR_INVALID_INPUT, "File empty.")
+            return
+        else if (iostat > 0) then
+            write(message, "(A,I0)") "Read failed on line: ", line_count
+            error = error_t(ERROR_READ_FAILED, message)
+            return
+        end if
+
+        columns = len_trim(line) + 2  ! Two extra columns for padding before the first and after the last column.
+        if (columns <= 2) then
+            write(message, "(A,I0,A)") "Invalid input: Line ", line_count, " is empty."
+            error = error_t(ERROR_INVALID_INPUT, message)
+            return
+        else if (columns > MAX_COLUMNS) then
+            write(message, "(A,I0,A)") "Invalid input: Line ", line_count, " contains too many columns."
+            error = error_t(ERROR_INVALID_INPUT, message)
+            return
+        end if
+
+        temp_grid(:,:) = .false. ! Initialize temp grid to all `.false.` values. 
+        temp_grid(2, 2:columns - 1) = [(line(i:i) == "@", i=1, len_trim(line))]
+        do while(line_count + 2 < MAX_ROWS)
+            read(unit, "(A)", iostat=iostat) line
+            if (iostat < 0) then
+                exit
+            else if (iostat > 0) then
+                write(message, "(A,I0)") "Read failed on line: ", line_count + 1
+                error = error_t(ERROR_READ_FAILED, message)
+                return
+            end if
+            
+            if (len_trim(line) + 2 /= columns) then
+                write(message, "(A,I0)") "Wrong number of columns on line: ", line_count + 1
+                error = error_t(ERROR_INVALID_INPUT, message)
+                return
+            end if
+            line_count = line_count + 1
+
+            ! Fill temporary grid
+            temp_grid(line_count + 1, 2:columns - 1) = [(line(i:i) == "@", i=1, len_trim(line))]
+        end do
+        rows = line_count + 2 ! Two extra rows for padding before the first and after the last row.
+
+        ! Allocate just enough space to hold the grid, and copy `temp_grid` into it.
+        allocate(grid(rows,columns))
+        do i=1, columns
+            grid(1:rows, i) = temp_grid(1:rows, i)
+        end do
+    end subroutine read_scrolls
 end module m_scrolls
--- src/advent_of_code/day4/test_day4.f90
+++ src/advent_of_code/day4/test_day4.f90
@@ -1,6 +1,61 @@
 module test_day4
+    use iso_c_binding, only: c_null_char
     use assertions_gtest
     use m_error, only: error_t
     use m_scrolls
-    contains
+    implicit none
+contains
+    !$f90tw TESTCODE(TEST, test_day4, test_read_scrolls, test_read_scrolls,
+    subroutine test_read_scrolls() bind(C)
+        implicit none
+        integer :: unit, rows, columns
+        logical, allocatable :: grid(:,:)
+        type(error_t), allocatable :: error
+
+        ! Arrange
+        open(newunit=unit, status="scratch")
+        write(unit, "(A)") ".@.@.@.@."
+        write(unit, "(A)") "@.@.@.@.@"
+        write(unit, "(A)") ".@.@.@.@."
+        rewind(unit)
+
+        ! Act
+        call read_scrolls(unit, grid, rows, columns, error)
+        close(unit)
+
+        ! Assert
+        call F90_ASSERT_FALSE(allocated(error))
+        call F90_ASSERT_TRUE(allocated(grid))
+        call F90_ASSERT_EQ(rows, 5, "Expected 5 rows" // c_null_char)
+        call F90_ASSERT_EQ(columns, 11, "Expected 11 columns" // c_null_char)
+        call F90_ASSERT_TRUE(grid(:, 1) == .false., "Expected first column to be all false" // c_null_char)
+        call F90_ASSERT_TRUE(grid(:, columns) == .false., "Expected last column to be all false" // c_null_char)
+        call F90_ASSERT_TRUE(grid(1, :) == .false., "Expected first row to be all false" // c_null_char)
+        call F90_ASSERT_TRUE(grid(rows, :) == .false., "Expected last row to be all false" // c_null_char)
+    end subroutine test_read_scrolls
+    !$f90tw )
+
+    !$f90tw TESTCODE(TEST, test_day4, test_read_scrolls_row__column_mismatch, test_read_scrolls__column_mismatch,
+    subroutine test_read_scrolls__column_mismatch() bind(C)
+        implicit none
+        integer :: unit, rows, columns
+        logical, allocatable :: grid(:,:)
+        type(error_t), allocatable :: error
+
+        ! Arrange
+        open(newunit=unit, status="scratch")
+        write(unit, "(A)") ".@.@.@.@."
+        write(unit, "(A)") "@.@.@.@.@."
+        rewind(unit)
+
+        ! Act
+        call read_scrolls(unit, grid, rows, columns, error)
+        close(unit)
+
+        ! Assert
+        call F90_ASSERT_TRUE(allocated(error))
+        call F90_ASSERT_FALSE(allocated(grid))
+        call F90_ASSERT_EQ(error%code, ERROR_INVALID_INPUT)
+    end subroutine test_read_scrolls__column_mismatch
+    !$f90tw )
 end module test_day4
